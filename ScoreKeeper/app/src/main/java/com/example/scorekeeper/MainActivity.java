990f49bdc8b6a8ab3c86d6485a63b73745ce222e
sandeepkaur1234567890 <78116974+sandeepkaur1234567890@users.noreply.github.com>
2022-04-14 15:31:56 -0400

    Commit
    Few changes like added menu option and code for toast , new activity file

--- ScoreKeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
+++ ScoreKeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.scorekeeper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -15,8 +20,6 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Switch switch_btn;
-
 
     /* Here I declared variables */
     TextView t1, t2;
@@ -27,8 +30,31 @@ public class MainActivity extends AppCompatActivity {
     RadioGroup radioGroup;
     RadioButton radioButton;
 
+    private Switch switch_btn;
+    // here oncreateOptionMenu method is created
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
 
+    // here onOptionsItemSelected method is created
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.about){
+            //Toast code 
+            Toast.makeText(this, "Name:Sandeep Kaur Course Code:1009", Toast.LENGTH_SHORT).show();
+        }
+        if(id == R.id.setting){
 
+            Intent intent = new Intent(MainActivity.this,Setting.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,19 +62,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        /* Here the code for day and night mode starts */
+
         switch_btn=findViewById(R.id.switch_btn);
 
+        /* here declare setonchnagelistener method */
         switch_btn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+
+                /* when user checks this switch button at that time night mode is on */
                 if(isChecked){
                     getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                 }
+
+                /* when button is not activate at that time day mode is on  */
                 else{
                     getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 }
+
             }
+
+
         });
+
+
+
         /* Here I connected all variables to their IDs */
         t1= findViewById(R.id.scoreof1);
         t2 =findViewById(R.id.scoreof2);
